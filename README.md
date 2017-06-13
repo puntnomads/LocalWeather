273dca68d9d865246ea83918ce56578cbf00a01f
puntnomads <puntnomads@gmail.com>
2017-06-13 18:10:47 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+The page get geolocation data from the users browser and then displays the local weather to the user. The user is shown his city, the temperature, current weather type (e.g. rain, cloud, sun snow) and a background image based on the current weather type.
+
+Developed using JavaScript/jQuery and the Open Weather API.
+ 
---
+++ js/index.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+ getLocation();
+});
+
+var lon = null;
+var lat =  null;
+var tempInCelsius = null;
+var tempInFahrenheit = null;
+var tempUnit = 0;
+var mykey = config.API_KEY;
+
+function getLocation() {
+  if (navigator.geolocation) {      navigator.geolocation.getCurrentPosition(showPosition); }
+}
+
+function showPosition(position) {
+lat= position.coords.latitude;
+lon= position.coords.longitude;
+callOpenWeatherMapAPI();
+}
+function changeTempeatureUnit(){
+  if(tempUnit == 0){ $("#temperature").text(tempInFahrenheit);
+$('button').text('°F');
+tempUnit = 1;
+  } else { $("#temperature").text(tempInCelsius);
+$('button').text('°C');
+tempUnit = 0;
+  }
+}
+function callOpenWeatherMapAPI() {
+var url ="https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=" + mykey;
+$.getJSON(url,function(result){
+$("#city").text(result.name);
+tempInCelsius = Math.floor(result.main.temp-273.15);
+tempInFahrenheit = Math.floor(result.main.temp*9/5 -459.67);    $("#temperature").text(tempInCelsius);
+$("#description").text(result.weather[0].main);
+var imageUrl =  "http://openweathermap.org/img/w/"+result.weather[0].icon+".png";
+document.getElementById("myImage").src = imageUrl;
+});
+}
